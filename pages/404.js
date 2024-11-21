1179e8f5681a50bafd03f27451adf285b2c71452
Bruce <paydayucky@gmail.com>
2024-11-21 11:23:03 +0800

    init

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+
+export default function PageNotFound(props) {
+  return (
+	<>
+	  <div>404, Page Not Found</div>
+	</>
+  )
+}
---
+++ pages/500.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+
+export default function Error(props) {
+  return (
+	<>
+	  <div>Error, Data not found</div>
+	</>
+  )
+}
